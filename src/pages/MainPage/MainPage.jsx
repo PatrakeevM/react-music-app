59e8d29a57432eb5da8b00288f0a5625409106a5
unknown <patrakeev.matvej@bk.ru>
2023-11-26 14:05:00 +0300

    add basic layout

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,18 +1,44 @@
 import React from "react";
+import { Input } from "@mui/material";
 
 import Track from "../../components/Track/Track";
 import trackList from "../../assets/TrackList";
 import style from "./MainPage.module.scss";
 
-const MainPage = () => (
-  <div className={style.search}>
-    <>Поиск треков</>
-    <div className={style.list}>
-      {trackList.map((track) => (
-        <Track key={track.id} {...track} />
-      ))}
+const runSearch = (query) => {
+  if (!query) return trackList;
+
+  const lowerCaseQuery = query.toLowerCase();
+
+  return trackList.filter(
+    (track) =>
+      track.title.toLowerCase().includes(lowerCaseQuery) ||
+      track.artists.toLowerCase().includes(lowerCaseQuery)
+  );
+};
+
+const MainPage = () => {
+  const [tracks, setTracks] = React.useState(trackList);
+
+  const handleChange = (e) => {
+    const foundedTracks = runSearch(e.target.value);
+    setTracks(foundedTracks);
+  };
+
+  return (
+    <div className={style.search}>
+      <Input
+        className={style.input}
+        placeholder="Введите название трека"
+        onChange={handleChange}
+      />
+      <div className={style.list}>
+        {tracks.map((track) => (
+          <Track key={track.id} {...track} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MainPage;
